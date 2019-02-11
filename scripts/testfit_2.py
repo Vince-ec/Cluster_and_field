dad7852cf1a1cb5dd2af2c332e2ce27a55fe0856
Vince-ec <vestrada78840@gmail.com>
2019-02-11 15:10:32 -0600

    updates

---
+++ scripts/testfit_2.py
@@ -0,0 +1,110 @@
+#!/home/vestrada78840/miniconda3/envs/astroconda/bin/python
+from spec_exam import Gen_spec
+import numpy as np
+from glob import glob
+import pandas as pd
+import os
+import sys
+import fsps
+import dynesty
+from scipy.interpolate import interp1d, RegularGridInterpolator
+from sim_engine import forward_model_grism, Salmon
+from spec_id import Scale_model
+from spec_tools import Oldest_galaxy
+
+hpath = os.environ['HOME'] + '/'
+
+if hpath == '/home/vestrada78840/':
+    data_path = '/fdata/scratch/vestrada78840/data/'
+    model_path ='/fdata/scratch/vestrada78840/fsps_spec/'
+    chi_path = '/fdata/scratch/vestrada78840/chidat/'
+    spec_path = '/fdata/scratch/vestrada78840/stack_specs/'
+    beam_path = '/fdata/scratch/vestrada78840/beams/'
+    template_path = '/fdata/scratch/vestrada78840/data/'
+    out_path = '/home/vestrada78840/chidat/'
+    phot_path = '/fdata/scratch/vestrada78840/phot/'
+
+else:
+    data_path = '../data/'
+    model_path = hpath + 'fsps_models_for_fit/fsps_spec/'
+    chi_path = '../chidat/'
+    spec_path = '../spec_files/'
+    beam_path = '../beams/'
+    template_path = '../templates/'
+    out_path = '../data/posteriors/'
+    phot_path = '../phot/'
+    
+    
+sp = fsps.StellarPopulation(imf_type = 2, tpagb_norm_type=0, zcontinuous = 1, logzsol = np.log10(1), sfh = 4, tau = 0.1,
+                            dust_type = 1)
+
+Gs = Gen_spec('GND', 21156, 1.25378, g102_lims=[7900, 11500], g141_lims=[11100, 16500],
+                mdl_err = True, instr_err = True, phot_errterm = 0.03, decontam = True)  
+
+############
+###priors###
+def prior_transform(u):
+    m = (0.03 * u[0] + 0.001) / 0.019
+    a = 10. * u[1] + 0.1
+    t = 2*u[2] + 0.01
+    z = 2.5*u[3]
+    d = 2*u[4]
+    return [m, a, t, z, d]
+
+############
+#likelihood#
+def forward_model_all_beams(beams, trans, in_wv, model_wave, model_flux):
+    FL = np.zeros([len(beams),len(in_wv)])
+
+    for i in range(len(beams)):
+        mwv, mflx = forward_model_grism(beams[i], model_wave, model_flux)
+        FL[i] = interp1d(mwv, mflx)(in_wv)
+        FL[i] /= trans[i]
+
+    return np.mean(FL.T,axis=1)
+
+def Full_forward_model(spec, wave, flux, specz):
+    Bmfl = forward_model_all_beams(spec.Bbeam, spec.Btrans, spec.Bwv, wave * (1 + specz), flux)
+    Rmfl = forward_model_all_beams(spec.Rbeam, spec.Rtrans, spec.Rwv, wave * (1 + specz), flux)
+    Pmfl = spec.Sim_phot_mult(wave * (1 + specz),flux)
+
+    return Bmfl, Rmfl, Pmfl
+
+def Full_scale(spec, Pmfl):
+    PC = Scale_model(spec.Pflx, spec.Perr, Pmfl)
+    return PC
+
+def Full_fit(spec, Bmfl, Rmfl, Pmfl):
+
+    Bscale = Scale_model(spec.Bfl, spec.Ber, Bmfl)
+    Rscale = Scale_model(spec.Rfl, spec.Rer, Rmfl)
+
+    Bchi = np.sum(((((spec.Bfl/ Bscale) - Bmfl) / (spec.Ber / Bscale))**2))
+    Rchi = np.sum(((((spec.Rfl/ Rscale) - Rmfl) / (spec.Rer / Rscale))**2))
+    Pchi = np.sum((((spec.Pflx - Pmfl) / spec.Perr)**2))
+    
+    return Bchi, Rchi, Pchi
+
+def loglikelihood(X):
+    m,a,t,z,d= X
+    
+    sp.params['logzsol'] = np.log10( m )
+    sp.params['tau'] = t
+    sp.params['dust2'] = d
+    
+    wave, flux = sp.get_spectrum(tage = a, peraa = True)
+    Bmfl, Rmfl, Pmfl = Full_forward_model(Gs, wave, flux, z)
+    PC= Full_scale(Gs, Pmfl)
+    Bchi, Rchi, Pchi = Full_fit(Gs, PC * Bmfl, PC * Rmfl, PC * Pmfl)
+                  
+    return -0.5 * (Bchi + Rchi + Pchi)
+
+############
+####run#####
+dsampler = dynesty.DynamicNestedSampler(loglikelihood, prior_transform, ndim = 5, sample = 'rwalk') 
+dsampler.run_nested(wt_kwargs={'pfrac': 1.0}, dlogz_init=0.01, print_progress=False)
+
+dres = dsampler.results
+############
+####save####
+np.save(out_path + 'GND_21156_testfit_tmp_EF.npy', dres) 
