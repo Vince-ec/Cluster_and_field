424e93ea9f797601defc2e46861659aff95acf81
Vince-ec <vestrada78840@gmail.com>
2019-05-06 11:12:25 -0500

    updates

--- scripts/SFfit.py
+++ scripts/SFfit.py
@@ -12,51 +12,64 @@ if __name__ == '__main__':
     field = sys.argv[1] 
     galaxy = int(sys.argv[2])
     specz = float(sys.argv[3])
+    logmass = float(sys.argv[4])
     
 verbose=False
 poolsize = 8
 
-def Galfit_prior(u):
-    m = (0.03*u[0] +0.001 )/ 0.019
-    a = (3)* u[1] + 0.5
+def Z_prior_mu(lmass):
+    M = [9, 11.5]
+    P = [-0.5813, 0.06818]
+    iP = interp1d(M,P)
+    return iP(lmass) 
 
-    #t = Gaussian_prior(u[2], [0.01,3],1,.5)
+onesig = (0.04 + 0.47)/2
+
+if (Z_prior_mu(logmass) - 2*onesig) < np.log10(0.001 / 0.019):
+    mllim = np.log10(0.001 / 0.019)
+else:
+    mllim = Z_prior_mu(logmass) - 2*onesig
     
-    tsamp = np.array([u[2],u[3],u[4],u[5],u[6],u[7]])
-    taus = stats.t.ppf( q = tsamp, loc = 0, scale = 0.3, df =2.)
-    m1, m2, m3, m4, m5, m6 = logsfr_ratios_to_masses(logmass = 0, logsfr_ratios = taus, agebins = get_agebins(a, binnum = 6))
+if (Z_prior_mu(logmass) + 2*onesig) > np.log10(0.03 / 0.019):
+    mhlim = np.log10(0.031 / 0.019)
+else:
+    mhlim = Z_prior_mu(logmass) + 2*onesig
     
-    lm = Gaussian_prior(u[8], [8.0, 12.5], 11, 0.75)
+def Galfit_prior(u):
+    #m = (0.03*u[0] +0.001 )/ 0.019
+    m = 10**Gaussian_prior(u[0], [mllim, mhlim], Z_prior_mu(logmass), onesig)
+    a = (3)* u[1] + 0.01
+
+    t = Gaussian_prior(u[2], [0.01,3],1,.5)
+    
+    lm = Gaussian_prior(u[3], [8.0, 12.5], 11, 0.75)
   
     #z = stats.norm.ppf(u[4],loc = specz, scale = zscale)
     
-    d = 4*u[9]
+    d = 4*u[4]
     
-    bp1 = Gaussian_prior(u[10], [-0.1,0.1], 0, 0.05)
-    rp1 = Gaussian_prior(u[11], [-0.05,0.05], 0, 0.025)
+    bp1 = Gaussian_prior(u[5], [-0.1,0.1], 0, 0.05)
+    rp1 = Gaussian_prior(u[6], [-0.05,0.05], 0, 0.025)
     
-    ba = log_10_prior(u[12], [0.1,10])
-    bb = log_10_prior(u[13], [0.0001,1])
-    bl = log_10_prior(u[14], [0.01,1])
+    ba = log_10_prior(u[7], [0.1,10])
+    bb = log_10_prior(u[8], [0.0001,1])
+    bl = log_10_prior(u[9], [0.01,1])
     
-    ra = log_10_prior(u[15], [0.1,10])
-    rb = log_10_prior(u[16], [0.0001,1])
-    rl = log_10_prior(u[17], [0.01,1])
+    ra = log_10_prior(u[10], [0.1,10])
+    rb = log_10_prior(u[11], [0.0001,1])
+    rl = log_10_prior(u[12], [0.01,1])
    
     #lwa = get_lwa([m, a, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10], get_agebins(a),sp)[0]
     
-    return [m, a, m1, m2, m3, m4, m5, m6, lm, d, bp1, rp1, ba, bb, bl, ra, rb, rl]
+    return [m, a, t, lm, d, bp1, rp1, ba, bb, bl, ra, rb, rl]
 
 def Galfit_L(X):
-    m, a, m1, m2, m3, m4, m5, m6, lm, d, bp1, rp1, ba, bb, bl, ra, rb, rl = X
+    m, a, t, lm, d, bp1, rp1, ba, bb, bl, ra, rb, rl = X
     
     sp.params['dust2'] = d
     sp.params['logzsol'] = np.log10(m)
-    
-    time, sfr, tmax = convert_sfh(get_agebins(a,binnum = 6), [m1, m2, m3, m4, m5, m6], maxage = a*1E9)
+    sp.params['tau'] = t
 
-    sp.set_tabular_sfh(time,sfr) 
-    
     wave, flux = sp.get_spectrum(tage = a, peraa = True)
 
     Gmfl, Pmfl = Full_forward_model(Gs, wave, F_lam_per_M(flux,wave*(1 + specz), specz, 0, sp.stellar_mass)*10**lm, specz, 
@@ -67,7 +80,7 @@ def Galfit_L(X):
     return Full_fit_2(Gs, Gmfl, Pmfl, [ba,ra], [bb,rb], [bl, rl], wvs, flxs, errs)
 
 #########define fsps#########
-sp = fsps.StellarPopulation(zcontinuous = 1, logzsol = 0, sfh = 3, dust_type = 2)
+sp = fsps.StellarPopulation(zcontinuous = 1, logzsol = 0, sfh = 4, tau = 1, dust_type = 2)
 sp.params['dust1'] = 0
 
 ###########gen spec##########
@@ -78,7 +91,7 @@ Gs = Gen_SF_spec(field, galaxy, 1, g102_lims=[8200, 11300], g141_lims=[11200, 16
 wvs, flxs, errs, beams, trans = Gather_grism_data(Gs)
 
 #######set up dynesty########
-sampler = dynesty.DynamicNestedSampler(Galfit_L, Galfit_prior, ndim = 18, nlive_points = 4000,
+sampler = dynesty.DynamicNestedSampler(Galfit_L, Galfit_prior, ndim = 13, nlive_points = 4000,
                                          sample = 'rwalk', bound = 'multi',
                                          pool=Pool(processes=8), queue_size=8)
 
@@ -96,12 +109,12 @@ np.save(out_path + '{0}_{1}_SFfit'.format(field, galaxy), dres)
 
 ##save out P(z) and bestfit##
 
-params = ['m', 'a', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'lm', 'd', 'bp1', 'rp1', 'ba', 'bb', 'bl', 'ra', 'rb', 'rl']
+params = ['m', 'a', 't', 'lm', 'd', 'bp1', 'rp1', 'ba', 'bb', 'bl', 'ra', 'rb', 'rl']
 for i in range(len(params)):
     t,pt = Get_posterior(dres,i)
     np.save(pos_path + '{0}_{1}_SFfit_P{2}'.format(field, galaxy, params[i]),[t,pt])
 
-bfm, bfa, bfm1, bfm2, bfm3, bfm4, bfm5, bfm6, bflm, bfd, bfbp1, bfrp1, bfba, bfbb, bfbl, bfra, bfrb, bfrl = dres.samples[-1]
+bfm, bfa, bft, bflm, bfd, bfbp1, bfrp1, bfba, bfbb, bfbl, bfra, bfrb, bfrl = dres.samples[-1]
 
 np.save(pos_path + '{0}_{1}_SFfit_bfit'.format(field, galaxy),
-        [bfm, bfa, bfm1, bfm2, bfm3, bfm4, bfm5, bfm6, bflm, bfd, bfbp1, bfrp1, bfba, bfbb, bfbl, bfra, bfrb, bfrl, dres.logl[-1]])
+        [bfm, bfa, bft, bflm, bfd, bfbp1, bfrp1, bfba, bfbb, bfbl, bfra, bfrb, bfrl, dres.logl[-1]])
