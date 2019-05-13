4f1df0e11640c0abb9352b5633d2a3f6d7abb854
Vince-ec <vestrada78840@gmail.com>
2019-05-13 10:34:25 -0500

    updates

--- scripts/fullfit-test.py
+++ scripts/fullfit-test.py
@@ -12,33 +12,30 @@ if __name__ == '__main__':
     field = sys.argv[1] 
     galaxy = int(sys.argv[2])
     specz = float(sys.argv[3])
-    photerr = float(sys.argv[4])
-    runnum = int(sys.argv[5])
     
-verbose=True
+verbose=False
 poolsize = 8
 
-agelim = Oldest_galaxy(specz)
+a = Oldest_galaxy(specz)
+agebins = get_agebins(a)
+zscale = 0.035 * (1 + specz)
 
 def Galfit_prior(u):
-    m = (0.03*u[0] + 0.001) / 0.019
-    
-    a = (agelim - 1)* u[1] + 1
-    
-    tsamp = np.array([u[2],u[3],u[4],u[5],u[6],u[7],u[8],u[9], u[10], u[11]])
+    m = Gaussian_prior(u[0], [0.002,0.03], 0.019, 0.08)/ 0.019
 
+    tsamp = np.array([u[1],u[2],u[3],u[4],u[5],u[6],u[7], u[8], u[9],u[10]])
     taus = stats.t.ppf( q = tsamp, loc = 0, scale = 0.3, df =2.)
-
-    m1, m2, m3, m4, m5, m6, m7, m8, m9, m10 = logsfr_ratios_to_masses(logmass = 0, logsfr_ratios = taus, agebins = get_agebins(a)) * 1E9
+    m1, m2, m3, m4, m5, m6, m7, m8, m9, m10 = logsfr_ratios_to_masses(logmass = 0, logsfr_ratios = taus, agebins = agebins)
     
-    z = stats.norm.ppf(u[12],loc = specz, scale = 0.005)
+    lm = Gaussian_prior(u[11], [9.5, 12.5], 11, 0.75)
+  
+    z = stats.norm.ppf(u[12],loc = specz, scale = zscale)
     
-    d = u[13]
+    d = log_10_prior(u[13],[1E-3,2])
     
-    bp1 = Gaussian_prior(u[14], [-0.5,0.5], 0, 0.25)
+    bp1 = Gaussian_prior(u[14], [-0.1,0.1], 0, 0.05)
+    rp1 = Gaussian_prior(u[15], [-0.05,0.05], 0, 0.025)
     
-    rp1 = Gaussian_prior(u[15], [-0.5,0.5], 0, 0.25)
-        
     ba = log_10_prior(u[16], [0.1,10])
     bb = log_10_prior(u[17], [0.0001,1])
     bl = log_10_prior(u[18], [0.01,1])
@@ -46,48 +43,42 @@ def Galfit_prior(u):
     ra = log_10_prior(u[19], [0.1,10])
     rb = log_10_prior(u[20], [0.0001,1])
     rl = log_10_prior(u[21], [0.01,1])
-        
-    lwa = get_lwa([m, a, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10], get_agebins(a), sp)
-
-    return [m, a, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, z, d, bp1, rp1, ba, bb, bl, ra, rb, rl, lwa]
+   
+    lwa = get_lwa([m, a, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10], agebins,sp)[0]
+    
+    return [m, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, lm, z, d, bp1, rp1, ba, bb, bl, ra, rb, rl, lwa]
 
 def Galfit_L(X):
-    m, a, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, z, d, bp1, rp1, ba, bb, bl, ra, rb, rl, lwa = X
+    m, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, lm, z, d, bp1, rp1, ba, bb, bl, ra, rb, rl, lwa = X
     
     sp.params['dust2'] = d
     sp.params['dust1'] = d
     sp.params['logzsol'] = np.log10(m)
+    
+    time, sfr, tmax = convert_sfh(agebins, [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10], maxage = a*1E9)
 
-    time, sfr, tmax = convert_sfh(get_agebins(a), [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10], maxage = a*1E9)
-
-    sp.set_tabular_sfh(time,sfr)    
+    sp.set_tabular_sfh(time,sfr) 
     
     wave, flux = sp.get_spectrum(tage = a, peraa = True)
 
-    Gmfl, Pmfl = Full_forward_model(Gs, wave, flux, z, wvs, flxs, errs, beams, trans)
+    Gmfl, Pmfl = Full_forward_model(Gs, wave, F_lam_per_M(flux,wave*(1+z),z,0,sp.stellar_mass)*10**lm, z, 
+                                    wvs, flxs, errs, beams, trans)
        
-    Gmfl = Full_calibrate(Gmfl, [bp1, rp1], wvs)
-        
-    PC= Full_scale(Gs, Pmfl)
-
-    LOGL = Full_fit_2(Gs, Gmfl, PC*Pmfl, [ba,ra], [bb,rb], [bl, rl], wvs, flxs, errs)
-                 
-#     return -0.5 * (Pchi+Gchi)
-
-    return LOGL
+    Gmfl = Full_calibrate_2(Gmfl, [bp1, rp1], wvs, flxs, errs)
+   
+    return Full_fit_2(Gs, Gmfl, Pmfl, [ba,ra], [bb,rb], [bl, rl], wvs, flxs, errs)
 
 #########define fsps#########
-sp = fsps.StellarPopulation(imf_type = 2, tpagb_norm_type=0, zcontinuous = 1, logzsol = 0, sfh = 3, dust_type = 1)
+sp = fsps.StellarPopulation(zcontinuous = 1, logzsol = 0, sfh = 3, dust_type = 1)
 
 ###########gen spec##########
-Gs = Gen_spec(field, galaxy, 1, g102_lims=[8300, 11288], g141_lims=[11288, 16500],mdl_err = False,
-        phot_errterm = photerr, irac_err = photerr*2, decontam = True) 
+Gs = Gen_spec(field, galaxy, 1, phot_errterm = 0.04, irac_err = 0.08) 
 
 ####generate grism items#####
 wvs, flxs, errs, beams, trans = Gather_grism_data(Gs)
 
 #######set up dynesty########
-sampler = dynesty.DynamicNestedSampler(Galfit_L, Galfit_prior, ndim = 23, nlive_points = 4000,
+sampler = dynesty.DynamicNestedSampler(Galfit_L, Galfit_prior, ndim = 24, nlive_points = 4000,
                                          sample = 'rwalk', bound = 'multi',
                                          pool=Pool(processes=8), queue_size=8)
 
@@ -95,20 +86,18 @@ sampler.run_nested(wt_kwargs={'pfrac': 1.0}, dlogz_init=0.01, print_progress=Tru
 
 dres = sampler.results
 
-np.save(out_path + '{0}_{1}_tabfit_{2}'.format(field, galaxy, runnum), dres) 
+np.save(out_path + '{0}_{1}_tabfit_test'.format(field, galaxy), dres) 
 
 ##save out P(z) and bestfit##
-
-params = ['m', 'a','m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'z', 'd',
-         'bp1', 'rp1', 'ba', 'bb', 'bl', 'ra', 'rb', 'rl', 'lwa']
-
+params = ['m', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'lm',
+          'z', 'd', 'bp1', 'rp1', 'ba', 'bb', 'bl', 'ra', 'rb', 'rl', 'lwa']
 for i in range(len(params)):
     t,pt = Get_posterior(dres,i)
-    np.save(pos_path + '{0}_{1}_tabfit_{2}_P{3}'.format(field, galaxy, runnum, params[i]),[t,pt])
+    np.save(pos_path + '{0}_{1}_tabfit_test_P{2}'.format(field, galaxy, params[i]),[t,pt])
 
-bfm, bfa, bfm1, bfm2, bfm3, bfm4, bfm5, bfm6, bfm7, bfm8, bfm9, bfm10, bfz, bfd, \
+bfm, bfm1, bfm2, bfm3, bfm4, bfm5, bfm6, bfm7, bfm8, bfm9, bfm10, bflm, bfz, bfd,\
     bfbp1, bfrp1, bfba, bfbb, bfbl, bfra, bfrb, bfrl, bflwa= dres.samples[-1]
 
-np.save(pos_path + '{0}_{1}_tabfit_{2}_bfit'.format(field, galaxy, runnum),
-        [bfm, bfa, bfm1, bfm2, bfm3, bfm4, bfm5, bfm6, bfm7, bfm8, bfm9, bfm10, bfz, bfd,
+np.save(pos_path + '{0}_{1}_tabfit_test_bfit'.format(field, galaxy),
+        [bfm, bfm1, bfm2, bfm3, bfm4, bfm5, bfm6, bfm7, bfm8, bfm9, bfm10, bflm, bfz, bfd,
          bfbp1, bfrp1, bfba, bfbb, bfbl, bfra, bfrb, bfrl, bflwa, dres.logl[-1]])
